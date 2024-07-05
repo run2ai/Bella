98070bcde7a575866bda1d64415869028263d0fe
m528605 <minjeog0@gmail.com>
2024-07-05 15:18:20 +0900

    #3 시간초과

--- baekjoon/11659.py
+++ baekjoon/11659.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 N, M = map(int,input().split())
 num = list(map(int,input().split()))
 
+#시간초과
 for _ in range(M):
     i, j = map(int,input().split())
     print(sum(num[i-1:j]))
