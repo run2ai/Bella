2d45eb27de29c10fa257373c6415801f3fbd29c0
m528605 <minjeog0@gmail.com>
2024-07-05 16:25:26 +0900

    #3 solve 누적합

--- baekjoon/11659.py
+++ baekjoon/11659.py
@@ -1,10 +1,17 @@
+#누적합 기법 사용
 import sys
 input = sys.stdin.readline
 
 N, M = map(int,input().split())
 num = list(map(int,input().split()))
 
-#시간초과
+temp =[]
+sum_ = 0
+
+for i in num:
+    sum_ +=i
+    temp.append(sum_)
+
 for _ in range(M):
     i, j = map(int,input().split())
-    print(sum(num[i-1:j]))
+    print(temp[j]-temp[i-1])
